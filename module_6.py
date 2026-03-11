064b263531881089bbf3cd291aee0ff49dfb3516
Alexander <alexander.khrapko@gmail.com>
2026-03-11 14:14:21 +0200

    module_6

---
+++ module_6.py
@@ -0,0 +1,70 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) !=10 or not value.isdigit():
+            raise ValueError("Phone number must be of 10 digits")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                break
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                return True
+        return False
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AdressBook(UserDict):
+    def __init__(self):
+        super().__init__()
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        # у критеріях нічого не написано про delete
+        # return self.data.pop(name, None)  # None, если запись не найдена
+        return self.data.pop(name)
+
+    def __str__(self):
+        if not self.data:
+            return "No records found"
+        return "\n".join(str(record) for record in self.data.values())
+
